d118156050bf5857941d2a39f9256a85b037d663
natassyyya <ekid9746@gmail.com>
2026-07-20 15:49:29 +0700

    feat: simplify Agenda CPT fields by removing lokasi and deskripsi to exactly match the homepage widget

--- wp-content/themes/dprd-purbalingga/inc/meta-boxes/agenda.php
+++ wp-content/themes/dprd-purbalingga/inc/meta-boxes/agenda.php
@@ -20,8 +20,6 @@ function dprd_render_agenda_meta_box($post) {
     wp_nonce_field('dprd_save_agenda_meta', 'dprd_agenda_meta_nonce');
     $tanggal = get_post_meta($post->ID, 'tanggal', true);
     $waktu = get_post_meta($post->ID, 'waktu', true);
-    $lokasi = get_post_meta($post->ID, 'lokasi', true);
-    $deskripsi = get_post_meta($post->ID, 'deskripsi', true);
     ?>
     <table class="form-table">
         <tr>
@@ -36,19 +34,6 @@ function dprd_render_agenda_meta_box($post) {
             <td>
                 <input type="text" name="waktu" id="dprd_waktu" value="<?php echo esc_attr($waktu); ?>" placeholder="Contoh: 09.00 WIB - Selesai" class="regular-text">
                 <p class="description">Tulis jam pelaksanaan (misalnya: 09.00 WIB - Selesai, atau 10.00 - 12.00 WIB).</p>
-            </td>
-        </tr>
-        <tr>
-            <th><label for="dprd_lokasi">Lokasi (Opsional)</label></th>
-            <td>
-                <input type="text" name="lokasi" id="dprd_lokasi" value="<?php echo esc_attr($lokasi); ?>" placeholder="Contoh: Ruang Rapat Paripurna" class="large-text">
-                <p class="description">Tempat pelaksanaan acara.</p>
-            </td>
-        </tr>
-        <tr>
-            <th><label for="dprd_deskripsi">Keterangan Tambahan (Opsional)</label></th>
-            <td>
-                <textarea name="deskripsi" id="dprd_deskripsi" rows="3" class="large-text" placeholder="Tulis detail atau keterangan tambahan mengenai agenda jika ada..."><?php echo esc_textarea($deskripsi); ?></textarea>
             </td>
         </tr>
     </table>
@@ -71,11 +56,5 @@ add_action('save_post', function ($post_id) {
     }
     if (isset($_POST['waktu'])) {
         update_post_meta($post_id, 'waktu', sanitize_text_field($_POST['waktu']));
-    }
-    if (isset($_POST['lokasi'])) {
-        update_post_meta($post_id, 'lokasi', sanitize_text_field($_POST['lokasi']));
-    }
-    if (isset($_POST['deskripsi'])) {
-        update_post_meta($post_id, 'deskripsi', sanitize_textarea_field($_POST['deskripsi']));
     }
 });
